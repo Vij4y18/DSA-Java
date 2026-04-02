4caf61081a8021e872c9fb3f13bd30134f276768
Vij4y18 <vijaysivakumar675@gmail.com>
2026-04-02 22:36:25 +0530

    basics

--- Bit Manipulation/src/Bits_Easy/Basics.java
+++ Bit Manipulation/src/Bits_Easy/Basics.java
@@ -14,8 +14,24 @@ public class Basics {
         ans.reverse();
         return ans.toString();
     }
+
+    static int binary2Decimal (String n) {
+        int ans = 0;
+        int len = n.length();
+        int pow2 = 1;
+
+        for (int i=len-1;i>=0;i--) {
+            if (n.charAt(i) == '1') {
+                ans += pow2;
+            }
+            pow2 = pow2*2;
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         int num = 10;
+        String n = "1101";
         System.out.println(decimal2Binary(num));
+        System.out.println(binary2Decimal(n));
     }
 }
