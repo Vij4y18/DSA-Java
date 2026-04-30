6497c8959a5b594bb9b7b29ccb33031120075d84
Vij4y18 <vijaysivakumar675@gmail.com>
2026-04-30 21:28:36 +0530

    Solve Largest Rectangle in Histogram using brute approach

---
+++ Stack Queue/src/Monotonic_Stack_Queue/LargestRectinHist.java
@@ -0,0 +1,53 @@
+package Monotonic_Stack_Queue;
+
+import java.util.Stack;
+
+class Solution {
+
+    public int[] NSE (int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        int[] res = new int[arr.length];
+
+        for (int i=arr.length-1; i>=0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) st.pop();
+
+            res[i] = st.isEmpty() ? arr.length : st.peek();
+            st.push(i);
+        }
+        return res;
+    }
+
+    public int[] PSE (int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        int[] res = new int[arr.length];
+
+        for (int i=0; i<arr.length; i++) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) st.pop();
+
+            res[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+        return res;
+    }
+
+    public int largestRectangleArea(int[] heights) {
+        int[] pse = PSE(heights);
+        int[] nse = NSE(heights);
+        int maxArea = 0;
+
+        for(int i=0; i<heights.length; i++) {
+            maxArea = Math.max(maxArea, heights[i] * (nse[i] - pse[i] - 1));
+        }
+        return maxArea;
+    }
+}
+
+public class LargestRectinHist {
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        int[] heights = {2,1,5,6,2,3};
+
+        int ans = obj.largestRectangleArea(heights);
+        System.out.println(ans);
+    }
+}
