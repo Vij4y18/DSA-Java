bd849c22ac7dee391c6203e2ac0c9733d6d117fa
Vij4y18 <vijaysivakumar675@gmail.com>
2026-05-07 19:01:01 +0530

    Change wrong file name

--- Stack Queue/src/ImplmentaionProblems/Celebrity.java
+++ Stack Queue/src/ImplmentaionProblems/Celebrity.java
@@ -1,37 +1,4 @@
 package ImplmentaionProblems;
 
-import java.util.Stack;
-
-class StockSpanner {
-    Stack<int[]> st = new Stack<>();
-    int indx;
-    public StockSpanner() {
-        st = new Stack<>();
-        indx = -1;
-    }
-
-    public int next(int price) {
-        indx++;
-
-        while(!st.isEmpty() && st.peek()[0] <= price) {
-            st.pop();
-        }
-        int ans = indx - (st.isEmpty() ? -1 : st.peek()[1]);
-        st.push(new int[]{price,indx});
-        return ans;
-    }
-}
-
 public class Celebrity {
-    public static void main(String[] args) {
-        StockSpanner sp = new StockSpanner();
-
-        System.out.println(sp.next(100)); // 1
-        System.out.println(sp.next(80));  // 1
-        System.out.println(sp.next(60));  // 1
-        System.out.println(sp.next(70));  // 2
-        System.out.println(sp.next(60));  // 1
-        System.out.println(sp.next(75));  // 4
-        System.out.println(sp.next(85));  // 6
-    }
 }
