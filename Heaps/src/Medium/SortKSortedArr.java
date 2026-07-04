ba0ab5d4c60d156756fb3b9159a2b6c421d8f041
Vij4y18 <vijaysivakumar675@gmail.com>
2026-07-04 20:29:12 +0530

    Implement Sort K Sorted array problem fron Striver's Heap series

---
+++ Heaps/src/Medium/SortKSortedArr.java
@@ -0,0 +1,37 @@
+package Medium;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class SortKSortedArr {
+
+    static int[] sortKSortedArray (int[] nums, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        int index = 0;
+
+        for (int i=0; i<nums.length; i++) {
+            if (heap.size() < k+1) {
+                heap.offer(nums[i]);
+            } else {
+                nums[index] = heap.poll();
+                heap.offer(nums[i]);
+                index++;
+            }
+        }
+
+        while (!heap.isEmpty()) {
+            nums[index] = heap.peek();
+            heap.poll();
+            index++;
+        }
+
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {6, 5, 3, 2, 8, 10, 9};
+        int k = 3;
+
+        System.out.println(Arrays.toString(sortKSortedArray(nums, k)));
+    }
+}
