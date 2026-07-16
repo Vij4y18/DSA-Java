f3f35f58ed0980e2e20d83d285e7d3bafc7e1749
Vij4y18 <vijaysivakumar675@gmail.com>
2026-07-16 22:15:08 +0530

    Implement Hand of Straights problem MinHeap version

--- Heaps/src/Medium/HandofStraights.java
+++ Heaps/src/Medium/HandofStraights.java
@@ -1,23 +1,47 @@
 package Medium;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.PriorityQueue;
 
 public class HandofStraights {
 
     static boolean isNStraightHand (int[] hands, int groupSize) {
         if (hands.length % groupSize != 0) return false;
 
-        Arrays.sort(hands);
-        int cnt = 1;
-        for (int i=0; i<hands.length-1; i++) {
-            if (cnt % groupSize != 0) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        for (int num : hands) {
+            mpp.put(num, mpp.getOrDefault(num,0)+1);
+        }
+        for (int num : mpp.keySet()) {
+            heap.offer(num);
+        }
 
+        while (!heap.isEmpty()) {
+            int start = heap.peek();
+
+            for (int i=0; i<groupSize; i++) {
+                int card = start+i;
+
+                if (!mpp.containsKey(card)) {
+                    return false;
+                }
+
+                mpp.put(card,mpp.get(card)-1);
+            }
+
+            while (!heap.isEmpty() && mpp.get(heap.peek()) == 0) {
+                heap.poll();
             }
         }
+        return true;
     }
 
     public static void main(String[] args) {
         int[] hands = {1,2,3,6,2,3,4,7,8};
         int groupSize = 3;
+
+        System.out.println(isNStraightHand(hands, groupSize));
     }
 }
