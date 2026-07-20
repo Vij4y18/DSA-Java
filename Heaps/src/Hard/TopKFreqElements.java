c42dff7295d906dbef649312204ac90b36806953
Vij4y18 <vijaysivakumar675@gmail.com>
2026-07-20 22:22:56 +0530

    Solve Top K frequency elements using heap

--- Heaps/src/Hard/TopKFreqElements.java
+++ Heaps/src/Hard/TopKFreqElements.java
@@ -1,27 +1,41 @@
 package Hard;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TopKFreqElements {
-    static int findKthLargest (int[] nums, int k) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
 
-        for (int i=0; i<nums.length; i++) {
+    static int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> mpp = new HashMap<>();
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> Integer.compare(mpp.get(a), mpp.get(b)));
+
+        for (int num : nums) {
+            mpp.put(num,mpp.getOrDefault(num,0)+1);
+        }
+
+        for (int i : mpp.keySet()) {
             if (heap.size() < k) {
-                heap.offer(nums[i]);
-            } else if (nums[i] > heap.peek()) {
+                heap.offer(i);
+            } else if (mpp.get(i) > mpp.get(heap.peek())) {
                 heap.poll();
-                heap.offer(nums[i]);
+                heap.offer(i);
             }
         }
 
-        return heap.peek();
+        int[] res = new int[k];
+        for (int i=0; i<k; i++) {
+            res[i] = heap.poll();
+        }
+
+        return res;
     }
 
     public static void main(String[] args) {
         int[] nums = {3,2,1,5,6,4};
         int k = 2;
 
-        System.out.println(findKthLargest(nums, k));
+        System.out.println(Arrays.toString(topKFrequent(nums, k)));
     }
 }
