3e46b9b3e2f5097d72a6aaccb4035537ae90c7b4
Vij4y18 <vijaysivakumar675@gmail.com>
2026-07-23 21:07:59 +0530

    Solve Lemonade change problem using greedy approach

---
+++ Greedy Algorithm/src/Easy/LemonadeChange.java
@@ -0,0 +1,36 @@
+package Easy;
+
+public class LemonadeChange {
+
+    static boolean lemonadeChange(int[] bills) {
+        int fives = 0;
+        int tens = 0;
+
+        for (int bill : bills) {
+            if (bill == 5) {
+                fives++;
+            } else if (bill == 10) {
+                if (fives == 0) return false;
+
+                fives--;
+                tens++;
+            } else {
+                if (tens != 0 && fives != 0) {
+                    tens--;
+                    fives--;
+                } else if (fives >= 3) {
+                    fives -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] bills = {5,5,5,20,10,10};
+
+        System.out.println(lemonadeChange(bills));
+    }
+}
