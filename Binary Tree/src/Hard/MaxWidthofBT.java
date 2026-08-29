4e525de11da45c1640f51b9c658750f72b905806
Vij4y18 <vijaysivakumar675@gmail.com>
2026-08-29 23:05:48 +0530

    Fix max width of binary tree solution

--- Binary Tree/src/Hard/MaxWidthofBT.java
+++ Binary Tree/src/Hard/MaxWidthofBT.java
@@ -1,4 +1,62 @@
 package Hard;
 
+import Nodes.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Pair {
+    TreeNode node;
+    int index;
+
+    Pair (TreeNode node, int index) {
+        this.node = node;
+        this.index = index;
+    }
+}
+
 public class MaxWidthofBT {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        root.left.right.left = new TreeNode(6);
+        root.left.right.right = new TreeNode(7);
+
+        System.out.println(widthOfBinaryTree(root));
+    }
+
+    static int widthOfBinaryTree(TreeNode root) {
+        Queue<Pair> Q = new LinkedList<>();
+        int ans = Integer.MIN_VALUE;
+
+        Q.offer(new Pair(root, 0));
+
+        while (!Q.isEmpty()) {
+            int size = Q.size();
+            int firstIndex = 0;
+            int lastIndex = 0;
+
+            for (int i=0; i<size; i++) {
+                Pair curr = Q.poll();
+                TreeNode currNode = curr.node;
+                int index = curr.index;
+
+                if (i == 0) firstIndex = index;
+                if (i == size-1) lastIndex = index;
+
+                if (currNode.left != null) Q.offer(new Pair(currNode.left, 2*index+1));
+                if (currNode.right != null) Q.offer(new Pair(currNode.right, 2*index+2));
+            }
+
+            ans = Math.max(ans, lastIndex-firstIndex + 1);
+        }
+
+        return ans;
+    }
 }
